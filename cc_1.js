783b6e8dbc78ed49a64c963913d8011ed7bf3da9
Hient <hient@usf.edu>
2025-01-23 14:57:18 -0500

    Task 1

---
+++ cc_1.js
@@ -0,0 +1,7 @@
+let employeeName = "Hien Tran";
+const employeeID = 3713;
+var isActive = true;
+
+console.log("Employee Name:", employeeName, "Type:", typeof employeeName);
+console.log("Employee ID:", employeeID, "Type:", typeof employeeID);
+console.log("Is Active:", isActive, "Type:", typeof isActive);
