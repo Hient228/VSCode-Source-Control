40a41e4ae49c91cdedafd742ad8199c9cdd59174
Hient <hient@usf.edu>
2025-01-26 00:20:17 -0500

    Task 2 - Student Scores

--- cc_2.js
+++ cc_2.js
@@ -5,3 +5,11 @@ products.push("Winston"); // Add a new product
 products.pop(); // Remove the last product
 
 console.log("Updated Product List:", products);
+
+//Task 2 - Student Scores
+let scores = [80, 76, 99, 84, 100];
+scores[1] = 94;
+let total = scores.reduce((sum, score) => sum + score, 0);
+let average = total / scores.length;
+console.log("Updated Scores:", scores);
+console.log("Average Score:", average);
