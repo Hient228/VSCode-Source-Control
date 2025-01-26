7bc58400917d4c054fbbd20c81f21c595a77e150
Hient <hient@usf.edu>
2025-01-26 00:19:52 -0500

    Task 1 - Store Inventory

---
+++ cc_2.js
@@ -0,0 +1,7 @@
+//Task 1 -Store Inventory
+let products = ["Newport", "Kool", "Clipper", "Salem", "Westport"];
+
+products.push("Winston"); // Add a new product
+products.pop(); // Remove the last product
+
+console.log("Updated Product List:", products);
